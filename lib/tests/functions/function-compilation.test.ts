029dde0eab38c2c8e06c22b53b925f3700082318
lucaslos <lucas@sugestly.com>
2025-07-28 20:00:49 -0300

    Add test for compiling file with vindurFn and styled components

--- lib/tests/functions/function-compilation.test.ts
+++ lib/tests/functions/function-compilation.test.ts
@@ -19,3 +19,35 @@ test('compile file with exported function', async () => {
 
   expect(result.css).toMatchInlineSnapshot(`""`);
 });
+
+test('compile file with vindurFn and styledComponent', async () => {
+  const result = await transformWithFormat({
+    source: dedent`
+      import { vindurFn } from 'vindur'
+
+      export const spacing = vindurFn((size: number) => \`margin: \${size}px\`);
+
+      const Container = styled.div\`
+        margin: \${spacing(16)};
+      \`
+
+      export const StyledComponentsDemo = () => {
+        return <Container>Hello</Container>
+      }
+    `,
+  });
+
+  expect(result.code).toMatchInlineSnapshot(`
+    "import { vindurFn } from "vindur";
+    export const spacing = vindurFn((size: number) => \`margin: \${size}px\`);
+    const Container = styled.div\`
+      margin: \${spacing(16)};
+    \`;
+    export const StyledComponentsDemo = () => {
+      return <Container>Hello</Container>;
+    };
+    "
+  `);
+
+  expect(result.css).toMatchInlineSnapshot(`""`);
+});
