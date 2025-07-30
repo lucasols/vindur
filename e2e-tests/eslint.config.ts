6b2110ce877780fa620f549899e33fe0c08be1a8
lucaslos <lucas@sugestly.com>
2025-07-30 10:24:47 -0300

    Add format-all script to VSCode tasks and update ESLint configuration to disable '@ls-stack/use-top-level-regex' rule

--- e2e-tests/eslint.config.ts
+++ e2e-tests/eslint.config.ts
@@ -2,4 +2,7 @@ import { createBaseConfig } from '../eslint.config.base.js';
 
 export default createBaseConfig({
   extraIgnorePatterns: ['**/test-runs/**', '**/test-results/**'],
+  globalRules: {
+    '@ls-stack/use-top-level-regex': 0,
+  },
 });
