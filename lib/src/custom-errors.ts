e9bc45448f8f49303c965ab5854c4b1756f3c8ae
lucasols <email@lucassantos.net>
2025-09-06 23:18:23 -0300

    refactor: enhance TransformError and TransformWarning classes for improved error handling
    - Updated the TransformError and TransformWarning classes to utilize an options object for optional parameters, improving clarity and consistency in error reporting.
    - Ensured the ignoreInLint property is now explicitly defined as boolean | undefined for better type safety.
    - Refactored constructor logic to streamline the assignment of filename and ignoreInLint properties, enhancing maintainability and readability.

--- lib/src/custom-errors.ts
+++ lib/src/custom-errors.ts
@@ -2,43 +2,47 @@ import type { types as t } from '@babel/core';
 
 export class TransformError extends Error {
   loc: { column: number; line: number; filename?: string } | undefined;
-  ignoreInLint?: boolean;
+  ignoreInLint: boolean | undefined;
 
   constructor(
     message: string,
     loc: t.SourceLocation,
-    filename?: string,
-    ignoreInLint?: boolean,
+    options: {
+      filename?: string;
+      ignoreInLint?: boolean;
+    } = {},
   ) {
     super(message);
     this.name = 'TransformError';
     this.loc = {
       column: loc.start.column,
       line: loc.start.line,
-      filename: filename ?? loc.filename,
+      filename: options.filename ?? loc.filename,
     };
-    this.ignoreInLint = ignoreInLint;
+    this.ignoreInLint = options.ignoreInLint;
   }
 }
 
 export class TransformWarning {
   message: string;
   loc: { column: number; line: number; filename?: string };
-  ignoreInLint?: boolean;
+  ignoreInLint: boolean | undefined;
 
   constructor(
     message: string,
     loc: t.SourceLocation,
-    filename?: string,
-    ignoreInLint?: boolean,
+    options: {
+      filename?: string;
+      ignoreInLint?: boolean;
+    } = {},
   ) {
     this.message = message;
     this.loc = {
       column: loc.start.column,
       line: loc.start.line,
-      filename: filename ?? loc.filename,
+      filename: options.filename ?? loc.filename,
     };
-    this.ignoreInLint = ignoreInLint;
+    this.ignoreInLint = options.ignoreInLint;
   }
 
   toJSON() {
